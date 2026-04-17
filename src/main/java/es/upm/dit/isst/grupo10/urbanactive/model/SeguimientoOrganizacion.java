65ee380a6eb9e225eeed52e6049abc3dae1afc5d
Elena <elena.sanz.delgado@alumnos.upm.es>
2026-04-17 18:30:05 +0200

    Añadido el html y modificado para que funcione la app

--- src/main/java/es/upm/dit/isst/grupo10/urbanactive/model/SeguimientoOrganizacion.java
+++ src/main/java/es/upm/dit/isst/grupo10/urbanactive/model/SeguimientoOrganizacion.java
@@ -4,54 +4,65 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(
-    name = "seguimiento_organizacion",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"seguidor_id", "organizacion_id"})
-    }
+name = "seguimiento_organizacion",
+uniqueConstraints = {
+@UniqueConstraint(columnNames = {
+"seguidor_email_direccion",
+"organizacion_tipo",
+"organizacion_numero"
+})
+}
 )
 public class SeguimientoOrganizacion {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "seguidor_id", nullable = false)
-    private Usuario seguidor;
+@ManyToOne(optional = false, fetch = FetchType.LAZY)
+@JoinColumn(
+name = "seguidor_email_direccion",
+referencedColumnName = "direccion",
+nullable = false
+)
+private Usuario seguidor;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "organizacion_id", nullable = false)
-    private Organizacion organizacion;
+@ManyToOne(optional = false, fetch = FetchType.LAZY)
+@JoinColumns({
+@JoinColumn(name = "organizacion_tipo", referencedColumnName = "tipo", nullable = false),
+@JoinColumn(name = "organizacion_numero", referencedColumnName = "numero", nullable = false)
+})
+private Organizacion organizacion;
 
-    public SeguimientoOrganizacion() {
-    }
+public SeguimientoOrganizacion() {
+}
 
-    public SeguimientoOrganizacion(Usuario seguidor, Organizacion organizacion) {
-        this.seguidor = seguidor;
-        this.organizacion = organizacion;
-    }
+public SeguimientoOrganizacion(Usuario seguidor, Organizacion organizacion) {
+this.seguidor = seguidor;
+this.organizacion = organizacion;
+}
 
-    public boolean esValido() {
-        return seguidor != null && organizacion != null;
-    }
+public boolean esValido() {
+return seguidor != null && organizacion != null;
+}
 
-    public Long getId() {
-        return id;
-    }
+public Long getId() {
+return id;
+}
 
-    public Usuario getSeguidor() {
-        return seguidor;
-    }
+public Usuario getSeguidor() {
+return seguidor;
+}
 
-    public void setSeguidor(Usuario seguidor) {
-        this.seguidor = seguidor;
-    }
+public void setSeguidor(Usuario seguidor) {
+this.seguidor = seguidor;
+}
 
-    public Organizacion getOrganizacion() {
-        return organizacion;
-    }
+public Organizacion getOrganizacion() {
+return organizacion;
+}
 
-    public void setOrganizacion(Organizacion organizacion) {
-        this.organizacion = organizacion;
-    }
+public void setOrganizacion(Organizacion organizacion) {
+this.organizacion = organizacion;
+}
 }

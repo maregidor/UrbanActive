b7d4bdcd133e72e1259e4d935400f2d2542e065f
Agussm <agus14sm@gmail.com>
2026-05-10 19:22:42 +0200

    Actualizacion securyty config

--- src/main/java/es/upm/dit/isst/grupo10/urbanactive/config/SecurityConfig.java
+++ src/main/java/es/upm/dit/isst/grupo10/urbanactive/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
             .requestMatchers("/login", "/css/**", "/js/**", "/images/**", "/h2-console/**").permitAll()
             .requestMatchers("/", "/actividades", "/actividades/{id}", "/register/**").permitAll()
 
-            .requestMatchers("/actividades/nueva", "/actividades/guardar").authenticated()
+            .requestMatchers("/actividades/nueva", "/actividades/guardar").hasAnyRole("USER", "ORGA")
             .requestMatchers("/mis-actividades/**", "/mi-perfil-organizacion").hasRole("ORGA")
 
             .requestMatchers("/reservas/**", "/mis-seguidos", "/mi-perfil").hasRole("USER")
